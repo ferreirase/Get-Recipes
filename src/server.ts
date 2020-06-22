b81d9f00cdef5e2fd4c702f88912fe0761c7153c
Anderson Raphael <phaell.anderson09@gmail.com>
2020-06-22 19:24:11 -0300

    feature(docker): included and configured Docker files

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import 'express-async-errors'; // pacote para tratamento de erros assíncronos
 import routes from './routes/index';
 import AppError from './errors/AppError';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 const app = express();
 
 app.use(express.json());
@@ -10,21 +13,26 @@ app.use(express.json());
 app.use(routes);
 
 // fazendo tratativa de erros de todas as rotas com um middleware
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  // se for um erro conhecido na minha aplicação
-  if (err instanceof AppError) {
-    return response
-      .status(err.statusCode)
-      .json({ status: 'error', message: err.message });
-  }
+app.use(
+  (err: Error, _request: Request, response: Response, _: NextFunction) => {
+    // se for um erro conhecido na minha aplicação
+    if (err instanceof AppError) {
+      return response
+        .status(err.statusCode)
+        .json({ status: 'error', message: err.message });
+    }
 
-  // se for um erro desconhecido, retorno uma mensagem genérica
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+    // se for um erro desconhecido, retorno uma mensagem genérica
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
-app.listen(3333, () => console.log('server started on port 3333'));
+const HOST = '0.0.0.0';
+const PORT = 3333;
+
+app.listen(PORT, HOST);
 
 export default app;
