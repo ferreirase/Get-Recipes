52b8839d68798a50b2266aff8e48fed19bd028d1
Anderson Raphael <phaell.anderson09@gmail.com>
2020-06-22 10:13:49 -0300

    feature(service): included and structured individual Gif search service

---
+++ src/services/GetOneGifService.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import AppError from '../errors/AppError';
+
+async function getOneGif(recipe: string): Promise<string> {
+  try {
+    const gif = await axios
+      .get(
+        `${process.env.BASE_URL_GIFS}?api_key=${
+          process.env.API_KEY_GIPHY
+        }&q=${recipe.trim()}`,
+      )
+      .then(response => response.data.data[0].url);
+
+    return gif;
+  } catch (error) {
+    throw new AppError({ message: error.message, statusCode: 400 });
+  }
+}
+
+export default getOneGif;
