4cf15fc51eb1e7a557d5812448f789395b1b08e8
Anderson Raphael <phaell.anderson09@gmail.com>
2020-06-22 10:23:30 -0300

    feature(middleware): included middleware on the main route

--- src/routes/recipes.routes.ts
+++ src/routes/recipes.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import FormatRecipeService from '../services/FormatRecipeService';
-// import verifyAmountIngredientsMiddleware from '../middlewares/verifyAmountIngredients';
+import verifyAmountIngredientsMiddleware from '../middlewares/verifyAmountIngredients';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
 const recipesRouter = Router();
 
-// recipesRouter.use(verifyAmountIngredientsMiddleware);
+recipesRouter.use(verifyAmountIngredientsMiddleware);
 
 recipesRouter.get('/', async (request, response) => {
   const keywords = request.query.i?.toString();
