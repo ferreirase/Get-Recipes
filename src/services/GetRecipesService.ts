f72b93d81f679d3bca25b84b35573ae33bde09ca
Anderson Raphael <phaell.anderson09@gmail.com>
2020-06-22 10:07:04 -0300

    feature(service): included and structured Recipe search service

---
+++ src/services/GetRecipesService.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import axios from 'axios';
+import AppError from '../errors/AppError';
+
+interface Recipe {
+  title: string;
+
+  ingredients: Array<string>;
+
+  href: string;
+
+  thumbnail: string;
+}
+
+async function getOneGif(keywords: string | undefined): Promise<Array<Recipe>> {
+  try {
+    const recipes = await axios
+      .get(`${process.env.BASE_URL_RECIPES}?i=${keywords}`)
+      .then(res => res.data.results);
+
+    return recipes;
+  } catch (error) {
+    throw new AppError({ message: error.message, statusCode: 400 });
+  }
+}
+
+export default getOneGif;
